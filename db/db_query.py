d9eec189f31a39b75ca496ba7d32d411ca040211
heko <91366057+hkohko@users.noreply.github.com>
2023-08-01 16:24:57 +0700

    initial commit

---
+++ db/db_query.py
@@ -0,0 +1,44 @@
+import sqlite3
+from parse_json import parse_file
+from os import chdir
+from pathlib import Path
+from tqdm import tqdm
+
+chdir(Path(__file__).parent.parent)
+
+
+def db_connect():
+    return sqlite3.connect("db/sqlite_db/tmdb.db")
+
+
+def create_table(conn: sqlite3.Connection):
+    cursor = conn.cursor()
+    Q_CREATE_TABLE = """CREATE TABLE IF NOT EXISTS tmdb(
+    id INTEGER PRIMARY KEY,
+    title TEXT
+    )
+    STRICT
+    """
+    cursor.execute(Q_CREATE_TABLE)
+
+
+def insert_into_tmdb(conn: sqlite3.Connection):
+    Q_INSERT_INTO_TMDD = """INSERT OR IGNORE INTO tmdb(
+    id,
+    title
+    ) VALUES(
+    :id,
+    :title
+    )
+    """
+
+    cursor = conn.cursor()
+    for id, title in tqdm(parse_file()):
+        cursor.execute(Q_INSERT_INTO_TMDD, (id, title))
+    conn.commit()
+    conn.close()
+
+
+if __name__ == "__main__":
+    create_table(db_connect())
+    insert_into_tmdb(db_connect())
---
+++ db/parse_json.py
@@ -0,0 +1,18 @@
+import json
+from typing import Generator
+from os import chdir
+from pathlib import Path
+from tqdm import tqdm
+
+chdir(Path(__file__).parent.parent)
+
+
+def parse_file() -> Generator[str, None, None]:
+    with open("db/movie_ids_05_15_2023.json", errors="ignore") as file:
+        for line in file:
+            if line is None:
+                break
+            text = json.loads(line)
+            tmdb_id: int = text.get("id")
+            tmdb_title: str = text.get("original_title")
+            yield tmdb_id, tmdb_title
