cdf42f902ffdadbd99a362f47f347ce1aa4e5817
heko <91366057+hkohko@users.noreply.github.com>
2023-08-15 16:54:40 +0700

    adding vxtwitter edgecases

--- main.py
+++ main.py
@@ -24,6 +24,13 @@ async def on_ready():
 async def on_message(msg):
     pattern = r"(\w+://)(\w+)(.com/\w+/.+)"
     check = re.match(pattern, msg.content)
+    try:
+        if "twitter" not in check.group(2):
+            await bot.process_commands(msg)
+            return
+    except AttributeError:
+        await bot.process_commands(msg)
+        return
     if check is not None and not msg.author.bot:
         if check.group(2) == "vxtwitter":
             await msg.channel.send(
@@ -34,7 +41,8 @@ async def on_message(msg):
         vx_twitter = re.sub(pattern, "\g<1>vxtwitter\g<3>", msg.content)
         await msg.delete()
         await msg.channel.send(f"{msg.author} sends:\n{vx_twitter}")
-    await bot.process_commands(msg)
+        await bot.process_commands(msg)
+        return
 
 
 @bot.command()
